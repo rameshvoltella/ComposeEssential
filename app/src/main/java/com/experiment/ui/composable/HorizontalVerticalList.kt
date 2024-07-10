4eeed8767b25568830048183f3b1168996dae3c3
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-07-10 11:02:42 +0530

    first commit

---
+++ app/src/main/java/com/experiment/ui/composable/HorizontalVerticalList.kt
@@ -0,0 +1,20 @@
+package com.experiment.ui.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun AppListItem()
+{
+
+ Column {
+
+     AsyncImage(modifier = Modifier.width(50.dp), contentDescription = "images", model = "https://play-lh.googleusercontent.com/gU9NKwpgLDYA6LIYK4dnkAkVyqNHUfTIqklEiNuO4oZ2OCpWQhQdqhnDh8Yb9B8SWIM=s96")
+
+ }
+
+}
